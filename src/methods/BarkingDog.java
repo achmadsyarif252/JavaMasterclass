756281c28ea6ef49656db33f16201ebdd1603eab
achmadsyarif252 <19kaizen@gmail.com>
2023-12-13 14:08:46 +0700

    feat : method exercise

---
+++ src/methods/BarkingDog.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if ((hourOfDay >= 0 && hourOfDay <= 23) && (hourOfDay < 8 || hourOfDay > 22) && barking) return true;
+        return false;
+
+         /*
+         other solution
+         if(hourOfDay < 0 || hourOfDay>23){
+             return false;
+         }
+         return barking && (hourOfDay<8||hourOfDay>22);
+         */
+    }
+}
